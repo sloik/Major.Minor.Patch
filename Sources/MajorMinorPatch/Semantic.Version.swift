272433a202b75fe1628728529416a3805e58a9d6
Edward Maliniak <mail@mail.com>
2022-09-08 23:56:07 +0200

    Adds addition and subtraction.

--- Sources/MajorMinorPatch/Semantic.Version.swift
+++ Sources/MajorMinorPatch/Semantic.Version.swift
@@ -10,3 +10,26 @@ extension Semantic {
     }
 
 }
+
+extension Semantic.Version: AdditiveArithmetic {
+
+    public static var zero: Semantic.Version {
+        .init(major: .zero, minor: .zero, patch: .zero)
+    }
+
+    public static func + (lhs: Semantic.Version, rhs: Semantic.Version) -> Semantic.Version {
+        .init(
+            major: lhs.major + rhs.major,
+            minor: lhs.minor + rhs.minor,
+            patch: lhs.patch + rhs.patch
+        )
+    }
+
+    public static func - (lhs: Semantic.Version, rhs: Semantic.Version) -> Semantic.Version {
+        .init(
+            major: lhs.major - rhs.major,
+            minor: lhs.minor - rhs.minor,
+            patch: lhs.patch - rhs.patch
+        )
+    }
+}
