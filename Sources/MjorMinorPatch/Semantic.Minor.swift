efbed129f636094cf7eec138ca408fd54ddd8ad6
Edward Maliniak <mail@mail.com>
2022-09-08 17:15:34 +0200

    Adds overload for pattern matching.

--- Sources/MjorMinorPatch/Semantic.Minor.swift
+++ Sources/MjorMinorPatch/Semantic.Minor.swift
@@ -58,6 +58,10 @@ public func ~= (pattern: Semantic.Minor, value: Semantic.Minor) -> Bool {
     pattern.rawValue == value.rawValue
 }
 
+public func ~= (pattern: UInt, value: Semantic.Minor) -> Bool {
+    pattern == value.rawValue
+}
+
 //MARK: - AdditiveArithmetic
 
 extension Semantic.Minor: AdditiveArithmetic {
