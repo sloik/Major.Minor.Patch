3345c56c33f68e3cde56aa91996001635c57edca
Edward Maliniak <mail@mail.com>
2022-09-10 22:23:59 +0200

    Ignores build metadata inc omperasion.

--- Sources/MajorMinorPatch/Semantic.swift
+++ Sources/MajorMinorPatch/Semantic.swift
@@ -40,6 +40,12 @@ extension Semantic: Comparable {
             }
 
             return lv < rv
+
+        /// Build metadata MUST be ignored when determining version precedence.
+        /// Thus two versions that differ only in the build metadata,
+        /// have the same precedence
+        case ( .vib(let lv, let li, _), .vib(let rv, let ri, _)):
+            return Semantic.vi(ver: lv, ids: li) < Semantic.vi(ver: rv, ids: ri)
             
         default:
             print("🛤", #function, #line)
