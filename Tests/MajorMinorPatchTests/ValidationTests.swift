d6d34956acf0c4f78bd8d349036b27b082d8a07e
Edward Maliniak <mail@mail.com>
2022-09-10 14:30:18 +0200

    RED: adds failing cases.

--- Tests/MajorMinorPatchTests/ValidationTests.swift
+++ Tests/MajorMinorPatchTests/ValidationTests.swift
@@ -16,13 +16,28 @@ final class ValidationTests: XCTestCase {
         XCTAssertTrue( isValidIdentifier("3000alpha") )
         XCTAssertTrue( isValidIdentifier("43724730237454032759234750932") )
 
+        
+        // Numeric identifiers MUST NOT include leading zeroes,
+        // so alphanumeric may contain them.
+        XCTAssertTrue( isValidIdentifier("0alpha") )
+        XCTAssertTrue( isValidIdentifier("00alpha") )
+        XCTAssertTrue( isValidIdentifier("000alpha") )
+        XCTAssertTrue( isValidIdentifier("a0") )
+        XCTAssertTrue( isValidIdentifier("00a") )
+        XCTAssertTrue( isValidIdentifier("0b00") )
+        XCTAssertTrue( isValidIdentifier("0c1") )
+        XCTAssertTrue( isValidIdentifier("00d1") )
+        XCTAssertTrue( isValidIdentifier("g0g0g0g1g") )
+
 
         // Invalid
         XCTAssertFalse( isValidIdentifier("") )
-
-        XCTAssertFalse( isValidIdentifier("0alpha") )
-        XCTAssertFalse( isValidIdentifier("00alpha") )
-        XCTAssertFalse( isValidIdentifier("000alpha") )
+        XCTAssertFalse( isValidIdentifier("0") )
+        XCTAssertFalse( isValidIdentifier("00") )
+        XCTAssertFalse( isValidIdentifier("000") )
+        XCTAssertFalse( isValidIdentifier("01") )
+        XCTAssertFalse( isValidIdentifier("001") )
+        XCTAssertFalse( isValidIdentifier("0001") )
         XCTAssertFalse( isValidIdentifier("043724730237454032759234750932") )
     }
 }
