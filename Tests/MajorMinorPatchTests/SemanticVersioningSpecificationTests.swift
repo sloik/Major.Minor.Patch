5bf8f61208e27cd6c6f1c820d203bcb5e9970f3b
Lukasz <stocki.lukasz@gmail.com>
2022-09-10 10:26:03 +0200

    Build (#6)
    * Refactors test.
    
    * Moves common values to SUTs.
    
    Co-authored-by: Edward Maliniak <mail@mail.com>

--- Tests/MajorMinorPatchTests/SemanticVersioningSpecificationTests.swift
+++ Tests/MajorMinorPatchTests/SemanticVersioningSpecificationTests.swift
@@ -15,40 +15,33 @@ final class SemanticVersioningSpecificationTests: XCTestCase {
     /// Example: 1.0.0 < 2.0.0 < 2.1.0 < 2.1.1.
     func test_verVer_comparison() {
 
-        // Arrange
-        let ver_2_0_0 = Semantic.v(ver: .init(major: 2, minor: 0, patch: 0) )
-        let ver_2_1_0 = Semantic.v(ver: .init(major: 2, minor: 1, patch: 0) )
-        let ver_2_1_1 = Semantic.v(ver: .init(major: 2, minor: 1, patch: 1) )
-
-
-        // Act & Assert
-        XCTAssert( Semantic.ver_0_0_0 < Semantic.ver_1_0_0 )
-        XCTAssert( Semantic.ver_0_0_0 < ver_2_0_0 )
-        XCTAssert( Semantic.ver_0_0_0 < ver_2_1_0 )
-        XCTAssert( Semantic.ver_0_0_0 < ver_2_1_1 )
-        XCTAssert( Semantic.ver_1_0_0 < ver_2_0_0 )
-        XCTAssert( Semantic.ver_1_0_0 < ver_2_1_0 )
-        XCTAssert( Semantic.ver_1_0_0 < ver_2_1_1 )
+        XCTAssert( ver_0_0_0 < ver_1_0_0 )
+        XCTAssert( ver_0_0_0 < ver_2_0_0 )
+        XCTAssert( ver_0_0_0 < ver_2_1_0 )
+        XCTAssert( ver_0_0_0 < ver_2_1_1 )
+        XCTAssert( ver_1_0_0 < ver_2_0_0 )
+        XCTAssert( ver_1_0_0 < ver_2_1_0 )
+        XCTAssert( ver_1_0_0 < ver_2_1_1 )
 
         XCTAssert( ver_2_0_0 < ver_2_1_0 )
         XCTAssert( ver_2_0_0 < ver_2_1_1 )
         XCTAssert( ver_2_1_0 < ver_2_1_1 )
 
-        XCTAssertFalse( Semantic.ver_1_0_0 < Semantic.ver_1_0_0 )
-        XCTAssertFalse( ver_2_0_0 < Semantic.ver_1_0_0 )
+        XCTAssertFalse( ver_1_0_0 < ver_1_0_0 )
+        XCTAssertFalse( ver_2_0_0 < ver_1_0_0 )
         XCTAssertFalse( ver_2_0_0 < ver_2_0_0 )
-        XCTAssertFalse( ver_2_1_0 < Semantic.ver_1_0_0 )
+        XCTAssertFalse( ver_2_1_0 < ver_1_0_0 )
         XCTAssertFalse( ver_2_1_0 < ver_2_0_0 )
         XCTAssertFalse( ver_2_1_0 < ver_2_1_0 )
-        XCTAssertFalse( ver_2_1_1 < Semantic.ver_1_0_0 )
+        XCTAssertFalse( ver_2_1_1 < ver_1_0_0 )
         XCTAssertFalse( ver_2_1_1 < ver_2_0_0 )
         XCTAssertFalse( ver_2_1_1 < ver_2_1_0 )
         XCTAssertFalse( ver_2_1_1 < ver_2_1_1 )
 
-        XCTAssertFalse( Semantic.ver_0_0_0 > Semantic.ver_1_0_0 )
-        XCTAssertFalse( Semantic.ver_0_0_0 > ver_2_0_0 )
-        XCTAssertFalse( Semantic.ver_0_0_0 > ver_2_1_0 )
-        XCTAssertFalse( Semantic.ver_0_0_0 > ver_2_1_1 )
+        XCTAssertFalse( ver_0_0_0 > ver_1_0_0 )
+        XCTAssertFalse( ver_0_0_0 > ver_2_0_0 )
+        XCTAssertFalse( ver_0_0_0 > ver_2_1_0 )
+        XCTAssertFalse( ver_0_0_0 > ver_2_1_1 )
     }
     
 }
