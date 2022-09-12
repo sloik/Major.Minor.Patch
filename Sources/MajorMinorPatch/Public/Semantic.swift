c9d4e9648a449289cdd84d1fa6705ab2ae138e44
Edward Maliniak <mail@mail.com>
2022-09-12 22:16:59 +0200

    Adds printing to version.

--- Sources/MajorMinorPatch/Public/Semantic.swift
+++ Sources/MajorMinorPatch/Public/Semantic.swift
@@ -141,3 +141,38 @@ public extension Semantic {
         .v(ver: Version(major: 1, minor: 0, patch: 0))
     }
 }
+
+// MARK: - LosslessStringConvertible
+
+extension Semantic: LosslessStringConvertible {
+
+    public var description: String {
+        switch self {
+
+        case .v(let ver):
+            return ver.description
+
+        case .vi(let ver, let ids):
+            let idsString: String = ids.map( \.description ).joined(separator: ".")
+            return ver.description + "-" + idsString
+
+        case .vb(let ver, let build):
+            let metadataString: String = build.map( \.description ).joined(separator: ".")
+            return ver.description + "+" + metadataString
+
+        case .vib(let ver, let ids, let build):
+            let idsString: String = ids.map( \.description ).joined(separator: ".")
+            let metadataString: String = build.map( \.description ).joined(separator: ".")
+
+            return ver.description + "-" + idsString + "+" + metadataString
+        }
+    }
+
+    public init?(_ description: String) {
+        do {
+            self = try Self.parser.parse(description[...])
+        } catch {
+            return nil
+        }
+    }
+}
